75d79540b22c2c7253ec12be3b617f6c94697822
stelaninja <stefan@johansson.com>
2021-12-11 00:02:01 +0100

    Finished day 8 part 1

---
+++ day8.py
@@ -0,0 +1,61 @@
+import os
+import re
+import sys
+import requests
+import numpy as np
+from collections import Counter
+from dotenv import load_dotenv
+
+load_dotenv("./.env")
+
+SESSION_KEY = {"session": os.environ.get("SESSION_KEY", None)}
+DAY = int(re.findall(r"[0-9]", sys.argv[0].split("/")[-1])[0])
+
+response = requests.get(
+    f"https://adventofcode.com/2021/day/{DAY}/input", cookies=SESSION_KEY
+)
+
+# TEST DATA
+TEST = False
+if TEST:
+    data = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
+
+else:
+    data = response.text
+
+data = [x.strip().split(" | ") for x in data.strip().split("\n")]
+
+inputs = [x[0] for x in data]
+outputs = [x[1] for x in data]
+
+code_dict = {
+    "1": 2,
+    "2": 5,
+    "3": 5,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 3,
+    "8": 7,
+    "9": 6,
+}
+
+result = 0
+
+for out in outputs:
+    o = out.split(" ")
+    for code in o:
+        if len(code) in [2, 4, 3, 7]:
+            result += 1
+
+
+print("Part 1:", result)
