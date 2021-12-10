9dbb816728a4f8d8e97bd3d177679a25c86d88cf
stelaninja <stefan@johansson.com>
2021-12-10 23:05:16 +0100

    Fixed a little

--- day7.py
+++ day7.py
@@ -15,12 +15,6 @@ response = requests.get(
     f"https://adventofcode.com/2021/day/{DAY}/input", cookies=SESSION_KEY
 )
 
-# TESTED ANSWERS
-# Part 2:
-# 98231561 (too low) round move
-# 98231570 (too low) round data
-# 98231739 (too high) round sum
-
 # TEST DATA
 TEST = False
 if TEST:
@@ -30,39 +24,17 @@ else:
 
 data = [int(x) for x in data.split(",")]
 
-# print(data)
-
 med = np.median(data)
 mean = np.mean(data)
-# print(med, med in data)
-print(mean)
 
 moves1 = []
 moves2 = []
 
 for crab in data:
     fuel1 = abs(crab - med)
-    # fuel2 = abs(crab - mean)
+    fuel2 = abs(crab - int(mean))
     moves1.append(fuel1)
-    # moves2.append(round((fuel2 * (fuel2 + 1)) / 2))
-
-minimum = 1e9
-position = None
-print(minimum)
-
-for i in range(int(mean) - 5, int(mean) + 5):
-    moves2 = []
-    for crab in data:
-        fuel = abs(crab - i)
-        moves2.append(round((fuel * (fuel + 1)) / 2))
-
-    res = sum(moves2)
-    # print(minimum, res)
-    if res < minimum:
-        position = i
-        minimum = res
-
-print(position)
+    moves2.append(round((fuel2 * (fuel2 + 1)) / 2))
 
 print("Part 1:", sum(moves1))
-print("Part 2:", minimum)
+print("Part 2:", sum(moves2))
